0bae1cf895b58126a957134c1fa845fb76a15a86
Miguel Saavedra <miguelernesto911@gmail.com>
2020-09-04 09:30:33 -0500

    created dynamodb

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "codimd-tfstate"
-    key    = "default-infrastructure.tfstate"
-    region = "us-east-1"
+    bucket  = "codimd-tfstate"
+    encrypt = true
+    key     = "default-infrastructure.tfstate"
+    region  = "us-east-1"
   }
 }
