b74474a16b00bf8174f2edb41a2906abc27cc47f
Miguel Saavedra <miguelernesto911@gmail.com>
2020-09-04 10:45:27 -0500

    sync (#15)
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * Develop infra (#6)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * sync: develop-infra to master (#8)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * Feature add actions (#7)
    
    * Develop infra (#6)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * added actions
    
    Co-authored-by: Miguel Saavedra <miguelernesto911@gmail.com>
    
    * sync: develop-infra to master (#10)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * Feature add actions (#7)
    
    * Develop infra (#6)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * added actions
    
    * Feature edit state (#9)
    
    * Develop infra (#6)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * sync: develop-infra to master (#8)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * Feature add actions (#7)
    
    * Develop infra (#6)
    
    * add infra (#1)
    
    * created dynamodb (#5)
    
    * add infra (#1) (#2)
    
    * Feature/add code (#3) (#4)
    
    * add infra (#1) (#2)
    
    * add code
    
    * Update readme
    
    * minor update
    
    * created dynamodb
    
    * edit
    
    * added actions
    
    Co-authored-by: Miguel Saavedra <miguelernesto911@gmail.com>
    
    * added dynamo table
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: Miguel Saavedra <miguelernesto911@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * update
    
    * Newfeature (#13)
    
    * some updates
    
    * deleted files
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "codimd-tfstate"
-    key    = "default-infrastructure.tfstate"
-    region = "us-east-1"
+    bucket          = "codimd-tfstate"
+    dynamodb_table  = "terraform-state-lock-dynamo"
+    encrypt         = true
+    key             = "default-infrastructure.tfstate"
+    region          = "us-east-1"
   }
 }
